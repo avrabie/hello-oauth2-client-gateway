b2cc23bf6b6da933e0e10771c29e7a2616755afc
moldo <44651279+avrabie@users.noreply.github.com>
2025-12-09 20:43:51 +0100

    i think the trick is to allow in the gateway public endpoints through the securityWebFilterChain

--- src/main/java/com/execodex/oauth2client/gateway/GateConfig.java
+++ src/main/java/com/execodex/oauth2client/gateway/GateConfig.java
@@ -5,30 +5,33 @@ import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class GateConfig {
 
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/public/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                ;
+        return http.build();
+    }
 
     @Bean
     RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder
                 .routes()
-
-//                .route(r -> r
-//                        .path("/interesting")
-//                        .filters(GatewayFilterSpec::tokenRelay)
-//                        .uri("http://localhost:8081/interesting")
-//                )
-//                .route(r -> r
-//                        .path("/int")
-//                        .filters(GatewayFilterSpec::tokenRelay)
-//                        .uri("http://localhost:8081/int")
-//                )
                 .route(r -> r
-                        .path("/public")
+                        .path("/public/**")
                         .uri("http://localhost:8081/public")
                 )
                 .route(r -> r
